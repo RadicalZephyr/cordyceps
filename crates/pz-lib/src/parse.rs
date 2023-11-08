8210367d04b26b59ce3724980b758a0bb02d40cf
Zefira Shannon <earthlingzephyr@gmail.com>
2023-11-08 02:58:35 -0800

    Allow leading whitespace

--- crates/pz-lib/src/parse.rs
+++ crates/pz-lib/src/parse.rs
@@ -1,13 +1,13 @@
 use nom::{
     bytes::complete::is_not,
     character::complete::{alpha1, char, multispace0},
-    sequence::{delimited, separated_pair, terminated},
+    sequence::{delimited, preceded, separated_pair, terminated},
     IResult,
 };
 
 fn item_attr<'a>(input: &'a str) -> IResult<&'a str, (&'a str, &'a str)> {
     separated_pair(
-        alpha1,
+        preceded(multispace0, alpha1),
         delimited(multispace0, char('='), multispace0),
         terminated(is_not(","), char(',')),
     )(input)
@@ -27,5 +27,9 @@ mod tests {
             Ok(("", ("DisplayName", "Campfire Materials"))),
             item_attr("DisplayName                     =        Campfire Materials,")
         );
+        assert_eq!(
+            Ok(("", ("Weight", "0.4"))),
+            item_attr("                Weight                          =               0.4,")
+        )
     }
 }
